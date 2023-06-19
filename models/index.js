83b79156d9a77696ea43dfe0cc1866ecf7b530d5
pedrohere <118082809+pedrohere@users.noreply.github.com>
2023-06-19 05:56:31 -0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 let dbURL = "mongodb+srv://" + process.env.USER + ":" + process.env.USER_KEY + "@" +
-    'synapsesoftwaredatabase.93xqzck.mongodb.net/?retryWrites=true&w=majority';
+    '';
 mongoose.connect(dbURL);
 
 let Client = require('./client');
