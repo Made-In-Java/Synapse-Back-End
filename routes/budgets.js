395617b3ffd72b7b7afec46122ef2e6c81ad7cbf
pedrohere <pedromatheus.dev@gmail.com>
2023-06-22 19:10:52 -0300

    Implement routing for budget data.

---
+++ routes/budgets.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var router = express.Router();
+let db = require('../models');
+
+/* GET all budget listing. */
+router.get('/', async (req, res, next) => {
+  try {
+    let allBudgets = await db.Budget.find();
+    res.json({ budgets: allBudgets });
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+/* GET one budget listing. */
+router.get('/:id', async (req, res, next) => {
+  try {
+    let budget = await db.Budget.findOne({ _id: req.params.id });
+    res.json({ budget });
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+/* POST a new budget */
+router.post('/', async (req, res, next) => {
+  try {
+    let newBudget = new db.Budget(req.body);
+    newBudget.createdAt = new Date();
+    let budget = await newBudget.save();
+    res.json({ budget });
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+/* PUT an update to a budget */
+router.put('/:id', async (req, res, next) => {
+  try {
+    let budgetId = req.params.id;
+    let newData = req.body;
+    newData.updatedAt = new Date();
+    let updatedBudget = await db.Budget.findByIdAndUpdate(budgetId, newData);
+    res.json(updatedBudget);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+/* DELETE a budget. */
+router.delete('/:id', async (req, res, next) => {
+  try {
+    let budgetId = req.params.id;
+    let removedBudget = await db.Budget.findOneAndRemove({ _id: budgetId }).populate('Budget').exec();
+    res.json(removedBudget);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
