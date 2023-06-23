1388b184bc37cc8adbef85735faf2e17c14779c8
pedrohere <118082809+pedrohere@users.noreply.github.com>
2023-06-22 21:41:03 -0300

    Add budget model

--- models/index.js
+++ models/index.js
@@ -4,5 +4,7 @@ let dbURL = "mongodb+srv://" + process.env.USER + ":" + process.env.USER_KEY + "
 mongoose.connect(dbURL);
 
 let Client = require('./client');
+let Budget = require('./budget');
 
 module.exports.Client = Client;
+module.exports.Budget = Budget;
