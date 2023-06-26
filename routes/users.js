e607c016ce8173f6cef74f98e626744e8541c8d5
pedrohere <118082809+pedrohere@users.noreply.github.com>
2023-06-26 13:53:42 -0300

    Implement missing CRUD routes.

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const db = require('../models');
 const auth = require('../middleware/auth');
 
 /* Get all users */
-router.get('/', auth.isAuthorized, async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     let users = await db.User.find({}).select('-password -loginToken -document');
     res.json({ users })
@@ -15,7 +15,7 @@ router.get('/', auth.isAuthorized, async (req, res, next) => {
 });
 
 /* Get single user */
-router.get('/:id', auth.isAuthorized, async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   try {
     let users = await db.User.findById(req.params.id).select('-password -loginToken -document');
     res.json({ users })
@@ -25,18 +25,42 @@ router.get('/:id', auth.isAuthorized, async (req, res, next) => {
 });
 
 /* Add a new User */
-router.post('/', auth.isAuthorized, async (req, res, next) => {
-
+router.post('/', async (req, res, next) => {
+  try {
+    let newClient = new db.Client(req.body);
+    newClient.createdAt = new Date();
+    newClient.save();
+    res.json({ newClient });
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 /* Update data from user */
-router.put('/:id', auth.isAuthorized, async (req, res, next) => {
-
+router.put('/:id', async (req, res, next) => {
+  try {
+    let userId = req.params.id;
+    let newData = req.body;
+    newData.updatedAt = new Date();
+    let updatedUser = await db.User.findByIdAndUpdate(userId, newData);
+    res.json(updatedUser);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 /* Delete user */
-router.delete('/:id', auth.isAuthorized, async (req, res, next) => {
-
+router.delete('/:id', async (req, res, next) => {
+  try {
+    let userId = req.params.id;
+    await db.Client.findOneAndRemove({ _id: userId }).populate('Client').exec();
+    res.status(200).json(removedClient);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
