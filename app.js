02d8bd1c60911524c3901ae66f66aec214bee386
pedrohere <118082809+pedrohere@users.noreply.github.com>
2023-06-26 13:49:05 -0300

    Implement missing routs and setting a host origin.

--- app.js
+++ app.js
@@ -2,15 +2,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var authRouter = require('./routes/auth');
-var clientsRouter = require('./routes/clients');
 var budgetsRouter = require('./routes/budgets');
+var clientsRouter = require('./routes/clients');
+var equipmentsRouter = require('./routes/equipments');
+var functionalitiesRouter = require('./routes/functionalities');
+var projectsRouter = require('./routes/projects');
+var servicesRouter = require('./routes/services');
+var usersRouter = require('./routes/users');
+
+const auth = require('./middleware/auth');
 
 var app = express();
 
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -18,12 +30,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/auth', authRouter);
-app.use('/clients', clientsRouter);
-//app.use('/projects', usersRouter);
-app.use('/budgets', budgetsRouter);
-//app.use('/equipments', usersRouter);
-//app.use('/services', usersRouter);
+app.use('/budgets', auth.isAuthorized, budgetsRouter);
+app.use('/clients', auth.isAuthorized, clientsRouter);
+app.use('/equipments', auth.isAuthorized, equipmentsRouter);
+app.use('/functionalities', auth.isAuthorized, functionalitiesRouter);
+app.use('/projects', auth.isAuthorized, projectsRouter);
+app.use('/services', auth.isAuthorized, servicesRouter);
+app.use('/users', auth.isAuthorized, usersRouter);
 
 module.exports = app;
