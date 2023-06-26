8bb761d2df39dcb7b4997992659976822e085904
pedrohere <118082809+pedrohere@users.noreply.github.com>
2023-06-26 13:45:01 -0300

    Check for jwt instead of UUIDs

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,34 +1,30 @@
 const crypto = require('crypto');
 const db = require('../models');
 
+const jwt = require('jsonwebtoken');
+const { json } = require('express');
+
 module.exports.isAuthorized = async (req, res, next) => {
 
-    var authCookie = req.cookies['auth'] || null;
-
+    var token = req.body.token || req.cookies?.token || null;
     var err = new Error('Not authorized');
     err.status = 401;
 
-    if (!authCookie || authCookie === '') {
+    if (!token || token === '') {
         err.message = "Bad cookie";
         return next(err);
     }
 
     try {
-        var user = await db.User.findOne({ 'loginToken.token': authCookie });
+        const payload = jwt.verify(token, process.env.TOKEN_KEY);
+        var user = await db.User.findById(payload.user._id);
     } catch (err) {
         err.message = "Bad connection";
         return next(err);
     }
 
     if (!user) {
-        err.message = "Bad cookie";
         return next(err);
     }
-
-    if (user.loginToken.expDate < new Date() || user.loginToken.token === '') {
-        err.message = "Bad cookie";
-        return next(err);
-    }
-
     return next();
 }
