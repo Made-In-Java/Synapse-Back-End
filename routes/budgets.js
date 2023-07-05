6f7384532562f63c6e0d405228c22550b39a914e
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2023-07-05 00:23:28 -0300

    [FIX] Client & Budget endpoint fixes

--- routes/budgets.js
+++ routes/budgets.js
@@ -1,64 +1,83 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-let db = require('../models');
+let db = require("../models");
 
 /* GET all budget listing. */
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     let allBudgets = await db.Budget.find();
     res.json({ budgets: allBudgets });
-  }
-  catch (err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 });
 
 /* GET one budget listing. */
-router.get('/:id', async (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   try {
     let budget = await db.Budget.findOne({ _id: req.params.id });
     res.json({ budget });
-  }
-  catch (err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 });
 
 /* POST a new budget */
-router.post('/', async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   try {
     let newBudget = new db.Budget(req.body);
-    newBudget.createdAt = new Date();
+
+    newBudget.createdAt = new Date().toISOString();
+    newBudget.updatedAt = new Date().toISOString();
+
+    newBudget.status = "PENDING";
+
+    const {
+      user: { _id: userId, name: userName },
+    } = req;
+
+    newBudget.historical = [
+      {
+        type: "ENTITY_CREATION",
+        description: "O orçamento foi cadastrado no sistema.",
+        date: new Date().toISOString(),
+        user: {
+          id: userId,
+          name: userName,
+        },
+      },
+    ];
+
     let budget = await newBudget.save();
+
     res.json({ budget });
-  }
-  catch (err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 });
 
 /* PUT an update to a budget */
-router.put('/:id', async (req, res, next) => {
+router.put("/:id", async (req, res, next) => {
   try {
     let budgetId = req.params.id;
     let newData = req.body;
     newData.updatedAt = new Date();
     let updatedBudget = await db.Budget.findByIdAndUpdate(budgetId, newData);
     res.json(updatedBudget);
-  }
-  catch (err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 });
 
 /* DELETE a budget. */
-router.delete('/:id', async (req, res, next) => {
+router.delete("/:id", async (req, res, next) => {
   try {
     let budgetId = req.params.id;
-    let removedBudget = await db.Budget.findOneAndRemove({ _id: budgetId }).populate('Budget').exec();
+    let removedBudget = await db.Budget.findOneAndRemove({ _id: budgetId })
+      .populate("Budget")
+      .exec();
     res.json(removedBudget);
-  }
-  catch (err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 });
--- routes/clients.js
+++ routes/clients.js
@@ -1,64 +1,82 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-let db = require('../models');
+let db = require("../models");
 
 /* GET all clients listing. */
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     let allClients = await db.Client.find();
     res.json({ clients: allClients });
-  }
-  catch (err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 });
 
 /* GET one client listing. */
-router.get('/:id', async (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   try {
     let client = await db.Client.findOne({ _id: req.params.id });
+
     res.json(client);
-  }
-  catch (err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 });
 
 /* POST a new client */
-router.post('/', async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   try {
     let newClient = new db.Client(req.body);
-    newClient.createdAt = new Date();
+
+    newClient.createdAt = new Date().toISOString();
+    newClient.createdAt = new Date().toISOString();
+
+    const {
+      user: { _id: userId, name: userName },
+    } = req;
+
+    newClient.historical = [
+      {
+        type: "ENTITY_CREATION",
+        description: "O cliente foi cadastrado no sistema.",
+        date: new Date().toISOString(),
+        user: {
+          id: userId,
+          name: userName,
+        },
+      },
+    ];
+
     newClient.save();
+
     res.json({ newClient });
-  }
-  catch (err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 });
 
 /* PUT update a client listing. */
-router.put('/:id', async (req, res, next) => {
+router.put("/:id", async (req, res, next) => {
   try {
     let clientId = req.params.id;
     let newData = req.body;
     newData.updatedAt = new Date();
     let updatedClient = await db.Client.findByIdAndUpdate(clientId, newData);
     res.json(updatedClient);
-  }
-  catch (err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 });
 
 /* DELETE delete a client listing. */
-router.delete('/:id', async (req, res, next) => {
+router.delete("/:id", async (req, res, next) => {
   try {
     let clientId = req.params.id;
-    let removedClient = await db.Client.findOneAndRemove({ _id: clientId }).populate('Client').exec();
+    let removedClient = await db.Client.findOneAndRemove({ _id: clientId })
+      .populate("Client")
+      .exec();
     res.json(removedClient);
-  }
-  catch (err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 });
